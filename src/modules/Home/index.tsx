52ddd46331bd8ec78d56563883ef20c5174f85cf
Chelzea Salvador <chelzea.salvador@ubidy.com>
2020-06-09 14:56:38 +0800

    setup antd

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from 'antd/lib/button';
 
 function Home() {
   return (
     <section className="home-section">
-      <h1>UIKIT Starter</h1>
+      <h1>UIKIT Starters</h1>
+      <Button type="primary">Button</Button>
     </section>
   );
 }
